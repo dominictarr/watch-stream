53ed52bb8715db71d739ec8a9a1ce9c08c100543
Dominic Tarr <dominic.tarr@gmail.com>
2012-09-16 00:39:03 +0200

    initial

---
+++ README.md
@@ -0,0 +1,68 @@
+# watch-stream
+
+`ls` meets `tail -f`
+
+``` js
+var watcher = require('watch-stream')
+
+watcher(process.cwd(), opts)
+  .search(/mp3$/) //returns a stream!
+  .on('data', console.log)
+
+```
+
+wraps [mikeal](https://github.com/mikeal/watch)
+
+## API
+
+### watcher(root, opts)
+returns a `WatchStream` on all the flies under `root`.
+
+### .search (string | RegExp)
+
+return a `WatchStream` of flies that match the string or regexp.
+
+data objects look like this:
+
+``` js
+{ name: 'Charisma/Disasteradio - Charisma - 05 Gravy Rainbow.mp3',
+  create: true,
+  type: 'file',
+  size: 5598948,
+  mtime: '2012-09-14T13:51:57.000Z',
+  ctime: '2012-09-14T14:37:51.000Z' }
+```
+`type` it taken off the stat object,
+see [fs.Stats](http://nodejs.org/api/fs.html#fs_class_fs_stats)
+it corrisponds thusly:
+``` js
+    return (
+        curr.isFile()            ? 'file'
+      : curr.isDirectory()       ? 'dir'
+      : curr.isBlockDevice()     ? 'blocks'
+      : curr.isCharacterDevice() ? 'chars'
+      : curr.isSymbolicLink()    ? 'symlink'
+      : curr.isFIFO()            ? 'fifo'
+      : curr.isSocket()          ? 'socket' 
+      :                            undefined
+    )
+```
+
+### .destroy
+
+stop listening on changes
+
+## known issues
+
+File watching is more or less broken in some way or another
+on each platfrom. 
+
+example: https://github.com/mikeal/watch/issues/36
+
+and there are other tricky cases, like when a directory is moved.
+(now, the dir file still exists, but the names for the it's files 
+are now all different)
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,127 @@
+var watch = require('watch')
+var fs = require('fs')
+var through = require('through')
+var Stream = require('stream')
+var join = require('path').join
+
+var watcher = module.exports =  function (root, options) {
+  options = options || {}
+  var files = {}
+  var emitter = new Stream()
+  emitter.files = files
+  emitter.readable = true
+
+  emitter.destroy = function () {
+    for(var k in files)
+      fs.unwatchFile(join(root, k))
+    emitter.removeAllListeners('data')
+  }
+
+  function type (curr) {
+    return (
+        curr.isFile()            ? 'file'
+      : curr.isDirectory()       ? 'dir'
+      : curr.isBlockDevice()     ? 'blocks'
+      : curr.isCharacterDevice() ? 'chars'
+      : curr.isSymbolicLink()    ? 'symlink'
+      : curr.isFIFO()            ? 'fifo'
+      : curr.isSocket()          ? 'socket' 
+      :                            undefined
+    )
+  }
+
+  function desc(name, stat) {
+    return {
+      name : name,
+      type : type(stat),
+      size : stat.size,
+      mtime: stat.mtime,
+      ctime: stat.ctime,
+      atime: stat.ctime,
+    }
+  }
+
+  function merge (o, n) {
+    o.size  = n.size
+    o.mtime = n.mtime
+    o.atime = n.atime
+    return o
+  }
+
+  emitter.search = function (query) {
+    // query by iterating over all the filenames in memory.
+    // of course, an index would be better.
+    // but this is unlikely to be a bottleneck for a while...
+
+    var stream = through()
+
+    function test (file) {
+      return file.match(query) 
+    }
+
+    stream.on('close', function () {
+      emitter.removeListener('change', onChange)
+    })
+
+    function onChange (data) {
+      if(test(data.name)) {
+        stream.queue(data)
+      }
+    }
+    emitter.on('data', onChange)
+
+    //send the files that already exist.
+    process.nextTick(function () {
+      for(var name in files) {
+        onChange(files[name])
+      }
+    })
+
+    return stream
+  }
+
+  watch.createMonitor (root, options, function (monitor) {
+    var onCreate
+
+    function trim(name) {
+      return name.substring(root.length + 1)
+    }
+
+    monitor.on('created', onCreate = function (name, curr, prev) {
+      name = trim(name)
+      if(!files[name])
+        files[name] = desc(name, curr)
+      else
+        merge(files[name], desc(name, curr))
+      emitter.emit('data', files[name])
+    })
+
+    monitor.on('removed', function (name, curr, prev) {
+      name = trim(name)
+      var i 
+      files[name].delete = true
+      emitter.emit('data', files[name])
+      delete files[name]
+    })
+
+    monitor.on('changed', function (name, curr, prev) {
+      name = trim(name)
+      merge(files[name], curr)
+      emitter.emit('data', files[name])
+    })
+
+    for(var name in monitor.files)
+      onCreate(name, monitor.files[name])
+  })
+
+  return emitter
+}
+
+if(!module.parent)
+  var st = 
+  watcher (process.cwd(), {interval: 500})
+  var query = process.argv[3]
+  ;(query ? st.search(query) : st)
+    .on('data', function (data) {
+      console.log(JSON.stringify(data))
+    })
